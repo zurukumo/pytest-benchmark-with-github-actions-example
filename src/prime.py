f892b5f534fbed8f6cb90486320fec2a4a96c0d6
zurukumo <zurukumo@gmail.com>
2025-05-21 01:57:45 +0900

    feat: 速度を改善

--- src/prime.py
+++ src/prime.py
@@ -1,6 +1,11 @@
 def is_prime(n):
-    factors = []
-    for i in range(1, n + 1):
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(n**0.5) + 1, 2):
         if n % i == 0:
-            factors.append(i)
-    return len(factors) == 2
+            return False
+    return True
