8168b216031f64fc5cfed3d6fd29b0fb8d8a030d
github-action-benchmark <github@users.noreply.github.com>
2025-05-20 16:54:02 +0000

    add Benchmark (pytest) benchmark result for b62498fa6bfc4d53053bf4e8303a73f7f2d9f840

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1747760042618,
+  "repoUrl": "https://github.com/zurukumo/pytest-benchmark-with-github-actions-example",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "zurukumo@gmail.com",
+            "name": "zurukumo",
+            "username": "zurukumo"
+          },
+          "committer": {
+            "email": "zurukumo@gmail.com",
+            "name": "zurukumo",
+            "username": "zurukumo"
+          },
+          "distinct": true,
+          "id": "b62498fa6bfc4d53053bf4e8303a73f7f2d9f840",
+          "message": "Initial empty commit for gh-pages branch",
+          "timestamp": "2025-05-21T01:53:10+09:00",
+          "tree_id": "8b3638f0c9ac2fd51690432c9b9cfc6da83c1513",
+          "url": "https://github.com/zurukumo/pytest-benchmark-with-github-actions-example/commit/b62498fa6bfc4d53053bf4e8303a73f7f2d9f840"
+        },
+        "date": 1747760042130,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_prime.py::test_is_prime_benchmark",
+            "value": 1.8900786777078864,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0053153380648857055",
+            "extra": "mean: 529.0785043999904 msec\nrounds: 5"
+          }
+        ]
+      }
+    ]
+  }
+}
